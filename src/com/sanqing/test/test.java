f39f6f0b18ac5556993569bd1ef01534176e2ab0
damonbian01 <870680723@qq.com>
2015-08-13 10:46:43 +0800

    all

---
+++ src/com/sanqing/test/test.java
@@ -0,0 +1,12 @@
+package com.sanqing.test;
+
+import com.sanqing.util.PageUtil;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println(PageUtil.getEveryPage(0));
+
+	}
+
+}
